fa3e78f95da2844d4c14a270c3d6c18f4dc172e1
Segev <segev95@gmail.com>
2021-11-02 18:00:21 +0200

    stam

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+	int x,y;
+	scanf("%d %d" &x, &y);
+	printf("Armstrong numbers::");
+	for(int i = x; i <= y; i++) {
+		if(isArmstrong(i) == 1) {
+			printf(" ");
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	printf("Palindromes numbers:");
+	for(int i=x ; i <= y;i++) {
+		if(isPalindrome(i)==1) {
+			printf(" ");
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	printf("Prime numbers:");
+	for(int i=x ; i <= y;i++) {
+		if(isPrime(i) == 1) {
+			printf(" ");
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	printf("Strong numbers:");
+	for(int i=x ; i <= y;i++) {
+		if(isStrong(i) == 1) {
+			printf(" ");
+			printf("%d", i);
+		}
+	}
+}
