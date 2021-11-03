f37b6591262002fbb548a0522e1f6e51ccd5319d
Segev <segev95@gmail.com>
2021-11-03 16:18:16 +0200

    work

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#include "NumClass.h"
 
+/*
 int isArmstrong(int num) {
-	int lenght = lenght1(num);
+	int lenght = length1(num);
 	int f = 0;
 	for (int i = 0; i<=lenght ; i++) {
 		f += pow(num%10,lenght);
@@ -15,9 +17,9 @@ int isArmstrong(int num) {
 }
 
 int isPalindrome(int num) {
-	int lenght = lenght1(num);
-	int x = 0;
-	int y = 1;
+	int lenght = length1(num); //int lenght
+	int x = 0; // 
+	int y = 1; //
 	int temp = num;
 	for (int i = 0; i<=lenght; i++) {
 		x += (temp % 10) * y;
@@ -29,13 +31,14 @@ int isPalindrome(int num) {
 	}
 	return 0;
 }
-int lenght1 (int num) {
+// checking int length
+int length1 (int num) {
 	int x = 0;
 	while (num!=0)
 	{
 		x++;
-		num/10;
+		num /= 10;
 	}
 	return x;
-}
+}*/
 
