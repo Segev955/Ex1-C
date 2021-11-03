cc9fe6c1f7a36d7a267412d5c3421899606c46e9
Segev <segev95@gmail.com>
2021-11-03 16:51:13 +0200

    work

---
+++ NumClass.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int isArmstrong(int);
+int isPalindrome(int);
+int isPrime(int);
+int isStrong(int);
+int factorial(int);
+int reverse(int);
+int myArmstrong(int,int,int);
+int length1 (int);
