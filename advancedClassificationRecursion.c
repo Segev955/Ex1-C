38be93fc2010ee0d6880390c24f5f4257ee0df5e
Segev <segev95@gmail.com>
2021-11-03 16:17:03 +0200

    work

---
+++ advancedClassificationRecursion.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+
+
+/**this function is called by isArmstrong function and check out with recursion if 
+* a given number "n" is equal to the sum of is digit powered by the number of digits of n
+**/
+int myArmstrong(int n,int b, int c){// b represents the sum of the numbers
+	int length = length1(n);// the length of n with using log identity
+	if(length<=0) {// if the length is 0
+		return b;//return the sum
+	}
+	b += pow(n%10, c);//c represents the number of digits in n
+	return myArmstrong(n/10, b, c);//get the next digit in n and the sum and the number of digits
+        }
+        /**this function get number n and return 1 if it armstrong number and 0 if not*/
+int isArmstrong(int n){
+	int length = length1(n);
+	if(myArmstrong(n,0,length)==n)
+		return 1;
+	return 0;
+}
+/** this function checks out if a number is palindrome by checking out if the reversed number is equal to the number*/
+int isPalindrome(int n) {
+	if(n==reverse(n)) {
+		return 1;
+	}
+	return 0;
+}
+
+/**this function using recursion to reverse the number*/
+int reverse(int n){
+	int ld;//last digit
+	static int sum=0;
+	if(n!=0){
+		ld=n%10;//get the last digit 
+		sum=sum*10+ld;//add to the sum the last digit and mult by 10 so it will be the lsb
+		reverse(n/10);//recursive call to reverse
+	}
+	else {
+		return sum;
+	}
+	return sum;
+}
+int length1 (int num) {
+	int x = 0;
+	while (num!=0)
+	{
+		x++;
+		num /= 10;
+	}
+	return x;
+}
