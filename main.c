ebb5019735baadb48d1bc2032f01fa4eca9ebc55
Segev <segev95@gmail.com>
2021-11-04 13:37:31 +0200

    fix redefintion

--- main.c
+++ main.c
@@ -3,36 +3,36 @@
 
 int main() {
 	int x,y;
-	scanf("%d %d", &x, &y);
-	printf("Armstrong numbers::");
+	scanf("%d%d", &x, &y);
+	printf("The Armstrong numbers are:");
 	for(int i = x; i <= y; i++) {
 		if(isArmstrong(i) == 1) {
 			printf(" ");
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
-	printf("Palindromes numbers:");
+	printf("The Palindromes numbers are:");
 	for(int i=x ; i <= y;i++) {
 		if(isPalindrome(i)==1) {
 			printf(" ");
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
-	printf("Prime numbers:");
+	printf("The Prime numbers are:");
 	for(int i=x ; i <= y;i++) {
 		if(isPrime(i) == 1) {
 			printf(" ");
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
-	printf("Strong numbers:");
+	printf("The Strong numbers are:");
 	for(int i=x ; i <= y;i++) {
 		if(isStrong(i) == 1) {
 			printf(" ");
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 }
