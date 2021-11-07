71c5acd815e0bf34e11210a517be404bae8d6893
Segev955 <Segev95@gmail.comgit config --global user.email Segev95@gmail.com>
2021-11-07 21:00:42 +0200

    fix masho

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main() {
 		}
 	}
 	printf("\n");
-	printf("The Palindromes numbers are:");
+	printf("The Palindromes are:");
 	for(int i=x ; i <= y;i++) {
 		if(isPalindrome(i)==1) {
 			printf(" ");
